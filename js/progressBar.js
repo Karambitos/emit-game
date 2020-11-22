3a5669cfc860686a35004e9307475935aaab796b
Dmytro <tschernomord@gmail.com>
2020-11-22 13:32:06 +0100

    moveProgressBar

---
+++ js/progressBar.js
@@ -0,0 +1,16 @@
+import { bar } from './refs.js'
+
+const progressBarObject = {
+    barMargin: 0,
+    changeProgressBar(params) {
+        this.barMargin += 50
+        bar.style.marginLeft = this.barMargin + "px"
+    }
+}
+
+export {
+    progressBarObject
+}
+
+
+
