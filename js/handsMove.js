0f894eb43182ea37891c97d4f3f47416631274d6
AnastasiaRiabova <AnastasiaRiabova@users.noreply.github.com>
2020-11-22 15:04:27 +0100

    move both hands

--- js/handsMove.js
+++ js/handsMove.js
@@ -1,8 +1,19 @@
 addElement();
 const toMove = {
   count: 0,
+  hand: true,
 
-  toMoveHand(e) {
+  toChooseHand(e) {
+    if (this.hand) {
+      this.hand = false;
+      this.toMoveRightHand(e);
+    } else {
+      this.hand = true;
+      this.toMoveLeftHand(e);
+    }
+  },
+
+  toMoveRightHand(e) {
     let rightHand = document.querySelector(".right-hand");
     rightHand.style.top = e.clientY + "px";
     rightHand.style.left = e.clientX + "px";
@@ -10,8 +21,21 @@ const toMove = {
     setTimeout(function () {
       rightHand.style.top = "";
       rightHand.style.left = "";
-      rightHand.style.right = 18 + "%";
-      rightHand.style.bottom = -23 + "%";
+    }, 500);
+    let progress = document.querySelector(".progress-percent");
+    progress.textContent = `${(this.count += 25)}%`;
+  },
+
+  toMoveLeftHand(e) {
+    let leftHand = document.querySelector(".left-hand");
+
+    leftHand.style.top = e.clientY + "px";
+
+    leftHand.style.left = e.clientX - 200 + "px";
+
+    setTimeout(function () {
+      leftHand.style.top = "";
+      leftHand.style.left = "";
     }, 500);
     let progress = document.querySelector(".progress-percent");
     progress.textContent = `${(this.count += 25)}%`;
@@ -27,6 +51,6 @@ function addElement() {
   let allTargets = document.querySelectorAll(".target");
 
   allTargets.forEach((el) =>
-    el.addEventListener("click", (e) => toMove.toMoveHand(e))
+    el.addEventListener("click", (e) => toMove.toChooseHand(e))
   );
 }
