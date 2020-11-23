1957cfcbb7043757a14b23fb4d56911ce71d8d00
KarambitoS <pase4nikdv@gmail.com>
2020-11-23 15:57:27 +0200

    browser detect added

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ $(document).ready(() => {
         };
 
         box.addEventListener('mousemove', (e) => {
-            console.log(e.currentTarget);
+            // console.log(e.currentTarget);
 
             const end = {
                 // x: ((e.clientX / window.innerWidth) - 0.5) * 2,
@@ -60,7 +60,7 @@ $(document).ready(() => {
         };
 
         box.addEventListener('mousemove', (e) => {
-            console.log(e.currentTarget);
+            // console.log(e.currentTarget);
 
             const end = {
                 x: ((e.clientX / window.innerWidth) - 0.5) * 2,
@@ -76,5 +76,147 @@ $(document).ready(() => {
             raf = null;
         }
     })
+    /*
+     * browser detect
+     */
+    var BrowserDetect = {
+        init: function () {
+            this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
+            this.version = this.searchVersion(navigator.userAgent) || this.searchVersion(navigator.appVersion) || "an unknown version";
+            this.OS = this.searchString(this.dataOS) || "an unknown OS";
+            const unsupportedScrean = document.querySelector('.unsupported-box');
+            let sections = document.querySelectorAll('section');
+
+            console.log(`broeser ${this.browser}, version ${this.version}, os ${this.OS}`);
+            console.log($(window).width());
+
+            if ($(window).width() < 1025) {
+                openUnsupportedPopUp();
+                console.log('if');
+            } else {
+                closeUnsupportedPopUp();
+                console.log('else');
+            }
+            if (
+                this.browser === ('Chrome') && this.version >= 87 ||
+                this.browser === ('Safari') && this.version >= 14 ||
+                this.browser === ('Firefox') && this.version >= 82) return;
+
+            openUnsupportedPopUp();
+            closeUnsupportedPopUp();
+            /*
+             * IF browser is wrong
+             * hide all sections and show 
+             * div with unsupported massege
+             */
+            function openUnsupportedPopUp() {
+                sections.forEach((section) => {
+                    section.classList.add('unsupported-browser');
+                    console.log(section);
+                });
+                unsupportedScrean.classList.add('unsupported-browser');
+            }
+
+            function closeUnsupportedPopUp() {
+                sections.forEach((section) => {
+                    section.classList.remove('unsupported-browser');
+                });
+                unsupportedScrean.classList.remove('unsupported-browser');
+            }
+
+        },
+        searchString: function (data) {
+            for (var i = 0; i < data.length; i++) {
+                var dataString = data[i].string;
+                var dataProp = data[i].prop;
+                this.versionSearchString = data[i].versionSearch || data[i].identity;
+                if (dataString) {
+                    if (dataString.indexOf(data[i].subString) != -1) return data[i].identity;
+                } else if (dataProp) return data[i].identity;
+            }
+        },
+        searchVersion: function (dataString) {
+            var index = dataString.indexOf(this.versionSearchString);
+            if (index == -1) return;
+            return parseFloat(dataString.substring(index + this.versionSearchString.length + 1));
+        },
+        dataBrowser: [{
+            string: navigator.userAgent,
+            subString: "Chrome",
+            identity: "Chrome"
+        }, {
+            string: navigator.userAgent,
+            subString: "OmniWeb",
+            versionSearch: "OmniWeb/",
+            identity: "OmniWeb"
+        }, {
+            string: navigator.vendor,
+            subString: "Apple",
+            identity: "Safari",
+            versionSearch: "Version"
+        }, {
+            prop: window.opera,
+            identity: "Opera",
+            versionSearch: "Version"
+        }, {
+            string: navigator.vendor,
+            subString: "iCab",
+            identity: "iCab"
+        }, {
+            string: navigator.vendor,
+            subString: "KDE",
+            identity: "Konqueror"
+        }, {
+            string: navigator.userAgent,
+            subString: "Firefox",
+            identity: "Firefox"
+        }, {
+            string: navigator.vendor,
+            subString: "Camino",
+            identity: "Camino"
+        }, { // for newer Netscapes (6+)
+            string: navigator.userAgent,
+            subString: "Netscape",
+            identity: "Netscape"
+        }, {
+            string: navigator.userAgent,
+            subString: "MSIE",
+            identity: "Explorer",
+            versionSearch: "MSIE"
+        }, {
+            string: navigator.userAgent,
+            subString: "Gecko",
+            identity: "Mozilla",
+            versionSearch: "rv"
+        }, { // for older Netscapes (4-)
+            string: navigator.userAgent,
+            subString: "Mozilla",
+            identity: "Netscape",
+            versionSearch: "Mozilla"
+        }],
+        dataOS: [{
+            string: navigator.platform,
+            subString: "Win",
+            identity: "Windows"
+        }, {
+            string: navigator.platform,
+            subString: "Mac",
+            identity: "Mac"
+        }, {
+            string: navigator.userAgent,
+            subString: "iPhone",
+            identity: "iPhone/iPod"
+        }, {
+            string: navigator.platform,
+            subString: "Linux",
+            identity: "Linux"
+        }]
+
+    };
+
+    BrowserDetect.init();
+    $(window).resize(function () {
+        BrowserDetect.init();
+    });
 
 });
