fd52af35d3bc2d6129ee477c95574628271f3849
AnastasiaRiabova <AnastasiaRiabova@users.noreply.github.com>
2020-11-24 12:17:28 +0100

    change pict depends on round

--- js/animation.js
+++ js/animation.js
@@ -1,53 +1,48 @@
 "use strict";
 
 $(document).ready(() => {
-    // AOS.init();
-    setInterval(() => {
-        animateDiv();
-    }, 1000);
-    let text = $('h1.title').text();
-    let length = text.length;
-    let timeOut;
-    let character = 0;
-    typeWriter();
+  // AOS.init();
+  setInterval(() => {
+    animateDiv();
+  }, 1000);
+  let text = $("h1.title").text();
+  let length = text.length;
+  let timeOut;
+  let character = 0;
+  typeWriter();
 
-    function typeWriter() {
-        // console.log('hi');
+  function typeWriter() {
+    // console.log('hi');
 
-        timeOut = setTimeout(function () {
-            character++;
-            let type = text.substring(0, character);
-            $('h1.title').text(type);
-            typeWriter();
+    timeOut = setTimeout(function () {
+      character++;
+      let type = text.substring(0, character);
+      $("h1.title").text(type);
+      typeWriter();
 
-            if (character == length) {
-                clearTimeout(timeOut);
-            }
-        }, 100);
-    };
-
-
+      if (character == length) {
+        clearTimeout(timeOut);
+      }
+    }, 100);
+  }
 });
 
 function makeNewPosition() {
+  // Get viewport dimensions (remove the dimension of the div)
+  var h = $(window).height() - 400;
+  var w = $(window).width() - 400;
 
-    // Get viewport dimensions (remove the dimension of the div)
-    var h = $(window).height() - 400;
-    var w = $(window).width() - 400;
+  var nh = Math.floor(Math.random() * h);
+  var nw = Math.floor(Math.random() * w);
 
-    var nh = Math.floor(Math.random() * h);
-    var nw = Math.floor(Math.random() * w);
-
-    return [nh, nw];
-
+  return [nh, nw];
 }
 
 function animateDiv() {
-    // console.log($('.main-target'));
-    var newq = makeNewPosition();
-    console.log(newq);
-    $('.main-target').animate({ top: newq[0], left: newq[1] }, 1000, function () {
-        animateDiv();
-    });
-
-};
+  // console.log($('.main-target'));
+  var newq = makeNewPosition();
+  // console.log(newq);
+  $(".main-target").animate({ top: newq[0], left: newq[1] }, 1000, function () {
+    animateDiv();
+  });
+}
